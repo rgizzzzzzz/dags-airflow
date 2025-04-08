06e66401e518abab2c6b91af55c301fa2fef6374
Reginaldo Silva <reginaldo.silva@mindlabs.com.br>
2025-04-08 17:00:33 -0300

    att

--- dags/teste-dag.py
+++ dags/teste-dag.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from cosmos import DbtDag, ProjectConfig, ProfileConfig
-from cosmos.profiles import PostgresUserPasswordProfileMapping
 from cosmos.operators import DbtDocsOperator
+from cosmos.profiles import PostgresUserPasswordProfileMapping
 
+# Caminho para o seu projeto DBT
 dbt_project_path = "/opt/airflow/dags/dbt_tutorial"
 
+# Configuração do profile DBT
 airflow_db = ProfileConfig(
     profile_name="airflow_db",
     target_name="dev",
@@ -14,22 +16,25 @@ airflow_db = ProfileConfig(
     ),
 )
 
+# Criando a DAG com context manager para permitir adição de tasks extras
+from airflow import DAG
+
 with DbtDag(
-    dag_id="teste_dag",
+    dag_id="dbt_com_docs_dag",
     project_config=ProjectConfig(dbt_project_path),
     profile_config=airflow_db,
     schedule_interval="@daily",
     start_date=datetime(2023, 1, 1),
     catchup=False,
-    tags=["simple"],
-) as simple_dag:
+    tags=["dbt", "docs"],
+) as dag:
 
+    # Cria a task de gerar a documentação
     generate_docs = DbtDocsOperator(
         task_id="generate_dbt_docs",
         project_dir=dbt_project_path,
         profile_config=airflow_db,
-)
+    )
 
-simple_dag.task_group >> generate_docs
-
-
+    # Conecta a task docs para rodar depois do último grupo de tarefas dbt
+    dag.dbt_task_group >> generate_docs
