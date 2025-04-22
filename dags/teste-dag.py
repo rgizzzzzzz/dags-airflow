b821013cc942bfaf1b2557679efa0f46e57828c0
Reginaldo Silva <reginaldo.silva@mindlabs.com.br>
2025-04-22 15:09:05 -0300

    aaa

--- dags/teste-dag.py
+++ dags/teste-dag.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from cosmos import DbtDag, ProjectConfig, ProfileConfig
 from cosmos.profiles import PostgresUserPasswordProfileMapping
 from cosmos.operators import DbtDocsOperator
-from airflow.models.baseoperator import chain
 import shutil
 import os
 
@@ -23,8 +22,10 @@ airflow_db = ProfileConfig(
 # Função para copiar os arquivos gerados para o diretório de docs
 def copy_docs_to_local(project_dir: str, context: dict = None):
     target_dir = os.path.join(project_dir, "target")
-
+    
+    # Verificar se a pasta de saída do DBT existe
     if os.path.exists(target_dir):
+        # Copiar os arquivos necessários para o diretório /opt/docs
         for file_name in ["index.html", "manifest.json", "graph.gpickle", "catalog.json"]:
             src_path = os.path.join(target_dir, file_name)
             if os.path.exists(src_path):
@@ -36,21 +37,19 @@ def copy_docs_to_local(project_dir: str, context: dict = None):
 
 # Criando a DAG com o DBT
 with DbtDag(
-    dag_id="teste_dag",
+    dag_id="teste_dag",  # dag_id sempre deve vir primeiro
     project_config=ProjectConfig(dbt_project_path),
     profile_config=airflow_db,
     schedule_interval="@daily",
     start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=["simple"],
-) as simple_dag:
+) as simple_dag:  # Usando o contexto 'with' para garantir que a DAG seja criada corretamente
 
+    # Definindo a tarefa para gerar a documentação DBT e copiar os arquivos para /opt/docs
     generate_dbt_docs = DbtDocsOperator(
         task_id="generate_dbt_docs",
         project_dir=dbt_project_path,
         profile_config=airflow_db,
-        callback=lambda project_dir, **kwargs: copy_docs_to_local(project_dir, context=kwargs),
+        callback=lambda project_dir, **kwargs: copy_docs_to_local(project_dir, context=kwargs),  # Alterado para usar **kwargs
     )
-
-    # Encadeia todas as tarefas do grupo para que generate_dbt_docs seja executado por último
-    chain(*simple_dag.task_group.group_tasks.values(), generate_dbt_docs)
